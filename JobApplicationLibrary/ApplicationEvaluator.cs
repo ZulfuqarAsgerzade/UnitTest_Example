470d1609754b5bccc91cdedf093901580b11e760
Zulfuqar Asgerzade <37780533+ZulfuqarAsgerzade@users.noreply.github.com>
2023-02-27 12:27:22 +0400

    Add files via upload

---
+++ JobApplicationLibrary/ApplicationEvaluator.cs
@@ -0,0 +1,29 @@
+﻿using JobApplicationLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobApplicationLibrary
+{
+    public class ApplicationEvaluator
+    {
+        public ApplicationResult Evaluate(JobApplication form)
+        {
+            if (form.Applicant.Age < 18) return ApplicationResult.AutoRejected;
+            return ApplicationResult.AutoAccepted;
+        }
+
+    }
+
+    public enum ApplicationResult 
+    {
+        AutoRejected,
+        TransferredToHr,
+        TransferredToLead,
+        TransferredToCTO,
+        AutoAccepted
+    }
+
+}
---
+++ JobApplicationLibrary/Models/JobApplication.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobApplicationLibrary.Models
+{
+    public class JobApplication
+    {
+        public Applicant Applicant { get; set; } = null;
+        public int YearsOfExperience { get; set; }
+        public List<string> TechStack { get; set; }
+
+        public JobApplication() => TechStack = new List<string>();
+     
+
+
+    }
+}
