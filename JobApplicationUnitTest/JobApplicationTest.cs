b8d59b2dcbabaa6f150b642494292e77dac5e125
Zulfuqar Asgerzade <37780533+ZulfuqarAsgerzade@users.noreply.github.com>
2023-02-27 12:27:57 +0400

    Add files via upload

---
+++ JobApplicationUnitTest/JobApplicationTest.cs
@@ -0,0 +1,28 @@
+﻿using JobApplicationLibrary;
+using JobApplicationLibrary.Models;
+
+namespace JobApplicationUnitTest
+{
+    public class JobApplicationTest
+    {
+        [Test]
+        public void FunctionUnderTest_WithUnderAge_UnderCondition()
+        {
+            // Arrange
+            ApplicationEvaluator evaluator = new ApplicationEvaluator();
+            JobApplication form = new JobApplication()
+            {
+                Applicant = new Applicant()
+                {
+                    Age = 18
+                }
+            };
+
+            // Act
+            var result = evaluator.Evaluate(form);
+
+            // Assert
+            Assert.AreEqual(ApplicationResult.AutoRejected, result);
+        }
+    }
+}
